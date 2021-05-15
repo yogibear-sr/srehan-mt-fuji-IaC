b92774ae1f33b8803a23f3f53408661e2cd775e0
yogibear-sr <60042822+yogibear-sr@users.noreply.github.com>
2021-05-15 16:19:49 +0100

    changed name of tags

--- mount-fuji.tf
+++ mount-fuji.tf
@@ -1,3 +1,5 @@
+# mount-fuji IaC exercise
+# author: Subhash Rehan
 provider "aws" {
   region = "us-east-2"
 }
@@ -20,7 +22,7 @@ resource "aws_security_group" "mount-fuji-ssh-http" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["172.31.0.0/16"]
-    description = "Allow ssh access from Subhash IP only"
+    description = "Allow ssh access from servers on local LAN"
   }
 
   ingress {
@@ -38,6 +40,12 @@ resource "aws_security_group" "mount-fuji-ssh-http" {
     cidr_blocks     = ["0.0.0.0/0"]
     description = "Allow All outgoing access"
   }
+
+
+  tags = {
+        Name = "Project-mount-fuji"
+  }
+
 }
 
 # create EC2 instance on default subnet and do some post build tasks
@@ -55,21 +63,21 @@ resource "aws_instance" "mount-fuji" {
                 #
                 # web server setup
                 rm /etc/httpd/conf.d/welcome.conf
+                sed -i 's/README\*//g'  /etc/httpd/conf.d/autoindex.conf
                 local_ip=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
                 echo "$local_ip        mount-fuji"   >> /etc/hosts
                 html_folder="/var/www/mount-fuji"
                 [[ ! -e $html_folder ]] && mkdir $html_folder
                 # create archives of git repo's
                 cd $html_folder
-                for REPO in sre-fuji-pingcloud fuji-app-python_module sre-mt-fuji-misc
+                for REPO in srehan-mt-fuji-IaC srehan-mt-fuji-App_Module srehan-mt-fuji-docker
                     do
                          git clone https://yogibear-sr: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@github.com/yogibear-sr/$REPO.git
                          (cd $REPO/ ; git archive main --format=tgz --output=../$REPO.tgz)
+                         [[ -e $REPO/mount-fuji.conf ]] && cp $REPO/mount-fuji.conf /etc/httpd/conf.d
                          [[ -e $REPO ]] && rm -rf $REPO
                     done
                 echo -e "User-agent: *\nDisallow: /" > /var/www/mount-fuji/robots.txt
-                cp /var/www/mount-fuji/sre-mt-fuji-misc/mount-fuji.conf /etc/httpd/conf.d
-                sed -i 's/README\*//g'  /etc/httpd/conf.d/autoindex.conf
                 chown -R apache:apache $html_folder
                 # enable from boot and start web service
                 sudo systemctl start httpd
@@ -78,7 +86,7 @@ resource "aws_instance" "mount-fuji" {
 
 
   tags = {
-        Name = "mount-fuji-webserver"
+        Name = "Project-mount-fuji"
   }
 
 }
@@ -104,6 +112,10 @@ resource "aws_security_group" "mount-fuji-elbsg" {
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
     description = "Allow All outgoing access"
+  }
+
+  tags = {
+        Name = "Project-mount-fuji"
   }
 }
 
@@ -132,8 +144,9 @@ resource "aws_elb" "mount-fuji-elb" {
   idle_timeout                = 400
   connection_draining         = true
   connection_draining_timeout = 400
+
   tags = {
-    Name = "mount-fuji-elb"
+    Name = "Project-mount-fuji"
   }
 }
 
